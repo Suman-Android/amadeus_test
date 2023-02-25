7f898e786f53828dd1812b3c0ff8ea1065b0f93b
suman-android <suman93.verma@gmail.com>
2023-02-25 11:18:27 +0530

    Design UI

--- app/src/main/java/com/coforge/amadeus/MainActivity.kt
+++ app/src/main/java/com/coforge/amadeus/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.coforge.amadeus
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.coforge.amadeus.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    val mainViewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/com/coforge/amadeus/MainViewModel.kt
@@ -0,0 +1,14 @@
+package com.coforge.amadeus
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.coforge.amadeus.models.WeatherDataItem
+import com.google.gson.Gson
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.InputStream
+
+class MainViewModel : ViewModel() {
+
+}
